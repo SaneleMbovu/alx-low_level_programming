4f4d7365f7d828f1ef4b8357c0f40f28c29ace7d
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-17 02:42:15 -0800

    This a C programming using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * Always return 0 when the code runs successfully
  * */
 
-int main()
-    {
-	    printf("with proper grammar, but the outcome is a piece of art");
+int main(void)
+{
+	 printf("/""with proper grammar, but the outcome is a piece of art");
     
 	 return (0);
-    }
+}
