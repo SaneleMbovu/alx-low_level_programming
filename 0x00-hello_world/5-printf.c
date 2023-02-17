21bcea5d01ad3796b3a2c00c1d011344b07f044d
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-17 02:37:50 -0800

    This a C programming using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/* This is a comment
+ * Always return 0 when the code runs successfully
+ * */
+
+int main()
+    {
+	    printf("with proper grammar, but the outcome is a piece of art");
+    
+	 return (0);
+    }
