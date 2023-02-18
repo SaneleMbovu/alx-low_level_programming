885e0cb2799ea52bf5fca73ba97fa79068aaeff1
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-18 10:49:15 -0800

    Code that check if the last digit of a given number meets the conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,21 +13,22 @@ int main(void)
 {
 	int n;
 	int last_nr;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_nr = n % 10;
 	/* your code goes there */
 	if (last_nr > 5)
 {
-	printf("last digit of %d is %d and is greater than 5\n", n, last_nr);
+	printf("Last digit of %d is %d and is greater than 5\n", n, last_nr);
 }
 	else if (last_nr == 0)
 {
-	printf("last digit of %d is %d and is 0\n", n, last_nr);
+	printf("Last digit of %d is %d and is 0\n", n, last_nr);
 }
 	else if (last_nr < 6 && last_nr != 0)
 {
-	printf(" last digit of %d is %d and is less than 6 and not 0", n, last_nr);
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, last_nr);
 }	
 	return (0);
 }
