54a449da896d96dc518510fc24b8e6ef66e10c61
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-18 09:27:13 -0800

    Code that check if the last digit of a given number meets the conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,9 @@
 int main(void)
 {
 	int n;
-        int last_nr;
+        int last_nr= n % 10;
 	
-	last_nr = n % 10;	
+	/*last_nr = n % 10; */	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
