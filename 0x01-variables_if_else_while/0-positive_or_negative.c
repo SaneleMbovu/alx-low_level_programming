efad475b1012a2f71de1e732887583fded4adbd2
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-18 07:31:55 -0800

    A C program for checking if numbers if they are positive, negative or equal to zero.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: This code gives out a random number
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2\n;
+	/* your code goes there */
+	if (n > 0)
+{
+	printf("%d is postive\n", n);
+}
+	else if (n == 0)
+{
+	printf("%d is zero\n", n);
+}
+	else
+{
+	printf("%d is negative\n", n);
+}
+	return (0);
+}
