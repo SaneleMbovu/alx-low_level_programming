7ae769b0ed436007f3264f29852e0c387bba5103
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-19 12:01:57 -0800

    This code prints all base 10 numbers that are single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i)
+}
+	return (0);
+}
