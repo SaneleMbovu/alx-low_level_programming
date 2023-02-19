a0adee3026aa928bca5dafacb2e3a7fa6945d5e3
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-19 03:17:01 -0800

    This code prints all alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main- Entry Point
+ *
+ * Description: This code..
+ *
+ * Return: 0 Always (Successful)
+ */
+int main(void)
+{
+	char ch;
+	int i = 97;
+
+	while (i < 123 && i != 101 && i != 113);
+{
+	ch = i;
+	putchar(ch);
+	i++;
+}
+
+	putchar('\n')
+	return (0);
+}
+
+
