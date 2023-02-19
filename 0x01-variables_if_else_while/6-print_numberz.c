3d6f2bac354e980196c6cbf1ec7d20b7fb62a3e3
SaneleMbovu <Sanelembovu21@icloud.com>
2023-02-19 13:06:07 -0800

    This code prints numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry Point
+ *
+ * Description: This code..
+ *
+ * Return: 0 Always (Successful)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+{
+
+	putchar(i);
+	i++;
+}
+
+	putchar('\n');
+	return (0);
+}
+
